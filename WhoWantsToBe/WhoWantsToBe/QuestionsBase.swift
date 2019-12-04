d000941164a4913a334780283e9bcfa9b9f8c096
Aleksandr Fetisov <grashey@mail.ru>
2019-12-04 22:05:25 +0300

    сменил фон

---
+++ WhoWantsToBe/WhoWantsToBe/QuestionsBase.swift
@@ -0,0 +1,9 @@
+//
+//  QuestionsBase.swift
+//  WhoWantsToBe
+//
+//  Created by Aleksandr Fetisov on 04.12.2019.
+//  Copyright © 2019 Aleksandr Fetisov. All rights reserved.
+//
+
+import Foundation
