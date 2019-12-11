58a06b9b538e6cde99741d2f2fe46cd64d8cdac9
Aleksandr Fetisov <grashey@mail.ru>
2019-12-12 01:30:59 +0300

    GameView для MVC

---
+++ WhoWantsToBe/WhoWantsToBe/View/GameView.swift
@@ -0,0 +1,21 @@
+//
+//  GameView.swift
+//  WhoWantsToBe
+//
+//  Created by Aleksandr Fetisov on 11.12.2019.
+//  Copyright © 2019 Aleksandr Fetisov. All rights reserved.
+//
+
+import UIKit
+
+class GameView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
