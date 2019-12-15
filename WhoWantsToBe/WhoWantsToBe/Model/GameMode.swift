eec535879e77ec1e425223dfb206a6ead8ad95bf
Aleksandr Fetisov <grashey@mail.ru>
2019-12-15 03:00:33 +0300

    настройки для режима выдачи вопросов

---
+++ WhoWantsToBe/WhoWantsToBe/Model/GameMode.swift
@@ -0,0 +1,9 @@
+//
+//  GameMode.swift
+//  WhoWantsToBe
+//
+//  Created by Aleksandr Fetisov on 15.12.2019.
+//  Copyright © 2019 Aleksandr Fetisov. All rights reserved.
+//
+
+import Foundation
---
+++ WhoWantsToBe/WhoWantsToBe/Model/GameModeStrategy.swift
@@ -0,0 +1,9 @@
+//
+//  GameModeStrategy.swift
+//  WhoWantsToBe
+//
+//  Created by Aleksandr Fetisov on 15.12.2019.
+//  Copyright © 2019 Aleksandr Fetisov. All rights reserved.
+//
+
+import Foundation
