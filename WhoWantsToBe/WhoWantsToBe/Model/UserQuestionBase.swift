c31d3ad308c6c08a15e644f9d65fc31eb535380e
Aleksandr Fetisov <grashey@mail.ru>
2019-12-16 17:53:12 +0300

    вопросы юзера

---
+++ WhoWantsToBe/WhoWantsToBe/Model/UserQuestionBase.swift
@@ -0,0 +1,9 @@
+//
+//  UserQuestionBase.swift
+//  WhoWantsToBe
+//
+//  Created by Aleksandr Fetisov on 16.12.2019.
+//  Copyright © 2019 Aleksandr Fetisov. All rights reserved.
+//
+
+import Foundation
---
+++ WhoWantsToBe/WhoWantsToBe/View/UserQuestionViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  UserQuestionViewCell.swift
+//  WhoWantsToBe
+//
+//  Created by Aleksandr Fetisov on 16.12.2019.
+//  Copyright © 2019 Aleksandr Fetisov. All rights reserved.
+//
+
+import UIKit
+
+class UserQuestionViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
