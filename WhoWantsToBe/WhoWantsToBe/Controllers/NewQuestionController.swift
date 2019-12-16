ea8dc61c71d7ea78d6ae26fa6c346ac737c520e2
Aleksandr Fetisov <grashey@mail.ru>
2019-12-16 18:49:29 +0300

    макет NewQuestionView

---
+++ WhoWantsToBe/WhoWantsToBe/Controllers/NewQuestionController.swift
@@ -0,0 +1,35 @@
+//
+//  NewQuestionController.swift
+//  WhoWantsToBe
+//
+//  Created by Aleksandr Fetisov on 16.12.2019.
+//  Copyright © 2019 Aleksandr Fetisov. All rights reserved.
+//
+
+import UIKit
+
+class NewQuestionController: UIViewController {
+    
+    @IBOutlet weak var newQuestionTableView: UITableView!
+    @IBAction func addQuestion(_ sender: Any) {
+    }
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ WhoWantsToBe/WhoWantsToBe/View/NewQuestionViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  NewQuestionViewCell.swift
+//  WhoWantsToBe
+//
+//  Created by Aleksandr Fetisov on 16.12.2019.
+//  Copyright © 2019 Aleksandr Fetisov. All rights reserved.
+//
+
+import UIKit
+
+class NewQuestionViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
